25f9dcd38e0a2e6d9c5ff9908bb63a346f67732b
Sanjana Moturi <sanjanamoturi24@gmail.com>
2026-01-31 20:27:42 +0530

    Notebook implementation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="agri_chatbot",
+    version="0.1.0",
+    author="Sanjana Moturi",
+    author_email="sanjanamoturi24@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
